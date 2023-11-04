dbf0b9b12f578e612d1c8bc6e9a73bb6ae2558eb
Dongyeop <kdy2975@naver.com>
2023-11-04 11:23:39 +0900

    UI 자동화

--- Assets/Script/UI/UI_Button.cs
+++ Assets/Script/UI/UI_Button.cs
@@ -1,18 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    [SerializeField]
-    Text _text;
+    // [SerializeField]
+    // Text _text;
+
+    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+    enum Buttons {
+        PointButton
+        
+    }
+    enum Texts {
+        PointText,
+        ScoreText
+
+    }
+
+    enum GameObjects {
+        TestObject,
+    }
+
+    private void Start() {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
+
+        // Get<Text>((int)Texts.ScoreText).text = "Bind Test";
+        GetText((int)Texts.ScoreText).text = "Bind Test";
+    }
+
+    // void Bind<T>(Type type) where T : UnityEngine.Object {
+    //     String[] names = Enum.GetNames(type);
+
+    //     UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+    //     _objects.Add(typeof(T), objects);
+
+    //     for (int i = 0; i < names.Length; i++) {
+    //         if (typeof(T) == typeof(GameObject)) {
+    //             objects[i] = Util.FindChild(gameObject, names[i], true);
+    //         } else {
+    //             objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+    //         }
+
+    //         if (objects[i] == null) {
+    //             Debug.Log($"Failed to bind {names[i]}");
+    //         }
+    //     }
+    // }
+
+    // T Get<T>(int idx) where T: UnityEngine.Object {
+    //     UnityEngine.Object[] objects = null;
+    //     if (_objects.TryGetValue(typeof(T), out objects) == false ) {
+    //         return null;
+    //     }
+
+    //     return objects[idx] as T;
+    // }
+
+    // Text GetText(int idx) { return Get<Text>(idx); }
+
+    // Button GetButton(int idx) { return Get<Button>(idx); }
+
+    // Image GetImage(int idx) { return Get<Image>(idx); }
 
     int _score = 0;
     public void OnButtonClicked() {
-        Debug.Log("Button Clicked");
+        // Debug.Log("Button Clicked");
 
-        _score++;
-        _text.text = $"점수: {_score}점";
+        // _score++;
+        // _text.text = $"점수: {_score}점";
     }
 }
